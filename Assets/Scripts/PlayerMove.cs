aa265119d3677c2809f217d395f67bf0cc35163a
전의준-PC\Varunas <dragmonitor123>
2024-06-10 21:42:52 +0900

    0610

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] float playerSpeed;
     [SerializeField] float playerRunSpeed;
     [SerializeField] GameObject player;
+    [SerializeField] GameObject wallCheck; 
     [SerializeField] float runTime;
 
     Vector3 moveDir;
@@ -21,6 +23,13 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         playerMove();
+        checkGravity();
+        climbWall();
+    }
+
+    private void checkGravity()
+    {
+        cController.Move(Physics.gravity * Time.deltaTime);
     }
 
     private void playerMove()
@@ -28,5 +37,21 @@ public class PlayerMove : MonoBehaviour
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
         cController.Move(transform.rotation * moveDir * playerSpeed * Time.deltaTime);
+    }
+
+    private void climbWall()
+    {
+        RaycastHit hit;
+        bool check = false;
+        Quaternion rot = Quaternion.Euler(-90, 0, 0);
+        
+        if(Physics.Raycast(wallCheck.transform.position, Vector3.back, out hit, 0.3f, LayerMask.GetMask("Ground")) && check == false)
+        {
+            check = true;
+            transform.rotation = rot;
+            Debug.Log("»Æ¿Œ");
+        }
+
+        Debug.DrawRay(wallCheck.transform.position, Vector3.back * 0.3f, Color.red);
     }
 }
